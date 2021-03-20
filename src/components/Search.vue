0b39cc75412699ba240ccff24bbec925406d4b27
Marcin Zmuda-Trzebiatowski <s20501@pjwstk.edu.pl>
2021-03-20 13:11:08 +0100

    Poprawki do wyszukiwarki

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,14 +1,14 @@
 <template>
   <v-row no-gutters>
-    <v-col md="1" sm="12">
+    <v-col md="1" sm="12" class="mt-5">
       <v-text-field class="rounded-l-lg" outlined label="SearchBy" disabled> </v-text-field>
     </v-col>
 
-    <v-col md="2" sm="12">
+    <v-col md="2" sm="12" class="mt-5">
       <v-select v-model="selectedType" outlined :items="items"> </v-select>
     </v-col>
 
-    <v-col md="2" sm="12">
+    <v-col md="2" sm="12" class="mt-5">
       <v-text-field v-model="search" class="rounded-r-lg" outlined append-icon="search"> </v-text-field>
     </v-col>
   </v-row>
