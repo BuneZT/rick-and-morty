b0565f1ff2255b9cc854456c6bdd25db3ba5c5c5
Marcin Zmuda-Trzebiatowski <s20501@pjwstk.edu.pl>
2021-03-27 13:19:56 +0100

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true
+    browser: true
   },
   extends: ['plugin:vue/essential'],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    sourceType: 'module',
+    es2021: true
   },
   plugins: ['vue', '@typescript-eslint', 'vuetify'],
   rules: {
